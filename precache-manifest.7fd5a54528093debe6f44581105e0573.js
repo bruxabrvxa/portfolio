df29d761a9d1f9f8532ee871cc1291301f430edb
angelod1as <angelo@angelodias.com.br>
2019-06-26 21:51:41 -0300

    Updates

---
+++ precache-manifest.7fd5a54528093debe6f44581105e0573.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e2e3ff6db7d113f70d27f129c0a484",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/portfolio/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "3d32de4a3e2558b477f2",
+    "url": "/portfolio/static/js/main.a79ac6e8.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
